b72dcc541c758cd2d4c913e8a9ce2d15c04091f8
Yoelvis Oliveros <28690224+yoliveros@users.noreply.github.com>
2026-02-26 16:15:12 -0400

    c debuger dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,5 +1,9 @@
 return {
-  'mfussenegger/nvim-dap',
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio"
+  },
   opts = {
     configurations = {
       gdscript = {
@@ -17,27 +21,83 @@ return {
         program = function()
           return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
         end,
-      }
+      },
+      c = {
+        {
+          name = "Launch",
+          type = "gdb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          args = {},
+          cwd = "${workspaceFolder}",
+          stopAtBeginningOfMainSubprogram = false
+        },
+        {
+          name = "Select and attach to process",
+          type = "gdb",
+          request = "attach",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          pid = function()
+            local name = vim.fn.input('Executable name (filter): ')
+            return require("dap.utils").pick_process({ filter = name })
+          end,
+          cwd = '${workspaceFolder}'
+        },
+        {
+          name = 'Attach to gdbserver :1234',
+          type = 'gdb',
+          request = 'attach',
+          target = 'localhost:1234',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}'
+        }
+      },
     },
     adapters = {
       coreclr = {
         type = "executable",
         command = vim.fn.stdpath("data") .. "/mason/packages/netcoredbg/netcoredbg",
         args = { "--interpreter=vscode" }
-      }
+      },
+      gdb = {
+        type = "executable",
+        command = "gdb",
+        args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+      },
     }
   },
   config = function()
-    vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-    vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-    vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
-    vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-    vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
-    vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+    local dap = require("dap")
+    local dapui = require("dapui")
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set('n', '<F5>', function() dap.continue() end)
+    vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+    vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+    vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+    vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
+    vim.keymap.set('n', '<Leader>B', function() dap.set_breakpoint() end)
     vim.keymap.set('n', '<Leader>lp',
-      function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-    vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
-    vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+      function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+    vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
+    vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
     vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
       require('dap.ui.widgets').hover()
     end)
