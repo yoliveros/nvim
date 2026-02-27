741447e4822de7ccd1cb7aa3cb76b383d64b85c5
Yoelvis Oliveros <yoelvisoliveros@gmail.com>
2026-02-27 12:11:21 -0400

    removed stylua

--- lsp/luals.lua
+++ lsp/luals.lua
@@ -1,5 +1,12 @@
 return {
   cmd = { "lua-language-server" },
   filetypes = { "lua" },
-  root_markers = { ".luarc.json", ".luarc.jsonc", ".stylua.toml" }
+  root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml" },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      }
+    }
+  }
 }
