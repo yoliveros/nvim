a9acf4c88980134cadc3bcf2091837812114d8f6
Yoelvis Oliveros <yoelvisoliveros@gmail.com>
2026-04-10 09:37:02 -0400

    treesitter migration to main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,16 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
   build = ":TSUpdate",
   lazy = false,
   dependencies = {
     "nvim-treesitter/nvim-treesitter-context"
   },
-  opts = {
-    ensure_installed = {
-      "lua",
-      "c",
-      "go",
-      "javascript",
-      "typescript",
-      "html"
-    },
-    sync_install = false,
-    auto_install = true,
-    autopairs = {
-      enable = true
-    },
-    indent = {
-      enable = true
-    },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false
-    }
+  install = {
+    "lua",
+    "c",
+    "go",
+    "javascript",
+    "typescript",
+    "html"
   }
 }
